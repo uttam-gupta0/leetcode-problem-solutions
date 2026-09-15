911b59d20847ef5ba17d70a07a0051bb8ace373b
uttam-gupta0 <guptauttam499@gmail.com>
2026-09-15 08:58:16 +0530

    Remove Duplicates from Sorted Array - 2026-09-15T03:28:15.765Z

--- 26-remove-duplicates-from-sorted-array/solution.java
+++ 26-remove-duplicates-from-sorted-array/solution.java
@@ -1,17 +1,13 @@
-// 1 ms | 46.1 MB
+// 1 ms | 46.9 MB
 class Solution {
     public int removeDuplicates(int[] nums) {
-         if (nums.length == 0) return 0;
-
-        int i = 0; // pointer for unique elements
-
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+        int k = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k = k + 1;
             }
         }
-
-        return i + 1;
+        return k;
     }
 }
