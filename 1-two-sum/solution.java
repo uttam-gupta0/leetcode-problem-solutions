66a1ec8a72f268ecb59502e584f9ef64c07b757b
uttam-gupta0 <guptauttam499@gmail.com>
2026-09-16 10:32:49 +0530

    Two Sum - 2026-09-16T05:02:49.219Z

--- 1-two-sum/solution.java
+++ 1-two-sum/solution.java
@@ -1,18 +1,21 @@
-// 2 ms | 47.2 MB
+// 2 ms | 47.3 MB
+import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
 
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
             }
 
             map.put(nums[i], i);
         }
 
-        return new int[] {}; // never reached (given one solution exists)
+        return new int[]{};
     }
 }
