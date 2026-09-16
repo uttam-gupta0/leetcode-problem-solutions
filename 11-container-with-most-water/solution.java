3f112cb181ce6eb9d7f7233e2b533cbb8978305c
uttam-gupta0 <guptauttam499@gmail.com>
2026-09-16 09:05:10 +0530

    Container With Most Water - 2026-09-16T03:35:08.859Z

---
+++ 11-container-with-most-water/solution.java
@@ -0,0 +1,20 @@
+// 5 ms | 58.8 MB
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0, right = height.length-1;
+        int maxArea=0;
+
+        while(left<right)
+        {
+            int width = right -left;
+            int minHeight = Math.min(height[left], height[right]);
+            int area = width * minHeight;
+            maxArea= Math.max(maxArea, area);
+            if(height[left]<height[right])
+               left++;
+            else
+            right--;
+        }
+            return maxArea;
+    }
+}
